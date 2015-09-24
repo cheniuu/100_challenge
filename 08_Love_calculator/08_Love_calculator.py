17908f23d53b5211d80171c7d888a048b0d76152
wchesyx <wojciechx.chesy@intel.com>
2015-09-24 14:23:59 +0200

    08 love calculator complete (ugly)

--- 08_Love_calculator/08_Love_calculator.py
+++ 08_Love_calculator/08_Love_calculator.py
@@ -12,17 +12,48 @@ def love_calc():
     scores = []
 
     letters = list(sum)
-    print letters
+    # print letters
 
     for i in [name, 'loves', name_l]:
         for j in i.lower():
-            print j
+            # print j
             if j in letters:
                 scores.append(letters.count(j))
                 letters[:] = [l for l in letters if l != j]
 
-    print scores
+    # print scores
+    state = True
+    while state:
+        temp = []
+        len_score = len(scores)
+        int_score = int(''.join(str(a) for a in scores))
 
+        if int_score < 100:
+            # state = False
+            break
 
+        if len_score % 2 == 0:
+            for i in xrange(int(round(len(scores) / 2.0))):
+                sum_score = scores[i] + scores[-(i + 1)]
+                if len(str(sum_score)) > 1:
+                    sum_score = [int(a) for a in str(sum_score)]
+                    for j in xrange(len(sum_score)):
+                        temp.append(sum_score[j])
+                else:
+                    temp.append(sum_score)
+        else:
+            for i in xrange(len(scores) / 2):
+                sum_score = scores[i] + scores[-(i + 1)]
+                if len(str(sum_score)) > 1:
+                    sum_score = [int(a) for a in str(sum_score)]
+                    for j in xrange(len(sum_score)):
+                        temp.append(sum_score[j])
+                else:
+                    temp.append(sum_score)
+            # temp[:] = [int(x) for x in temp]
+            temp.append(scores[int(i) + 1])
+        scores = temp
+
+    print "Your love has %s%% chance to survive." % int_score
 
 love_calc()
